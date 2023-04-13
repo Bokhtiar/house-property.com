e1a28a196be7273b9e5413ac3bb2bb7c26b243f9
Bokhtiar <bokhtiartoshar1@gmail.com>
2023-04-13 14:20:59 +0600

    tenant database desing

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -4,8 +4,59 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tenant extends Model
 {
     use HasFactory;
+    protected $table = 'tenants';
+    protected $primaryKey = 'tenant_id';
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'contact_number',
+        'job',
+        'age',
+        'familly_member',
+        'email',
+        'password',
+
+        'p_address',
+        'p_country',
+        'p_state',
+        'p_city',
+        'p_zip_code',
+
+        'present_address',
+        'present_country',
+        'present_state',
+        'present_city',
+        'present_zip_code',
+
+        'property_id',
+        'unit_id',
+        'lease_start_date',
+        'lease_end_date',
+        'general_rent',
+        'security_deposit',
+        'late_fee',
+        'incident_recipt',
+        'payment_due_on_date',
+        'document'
+
+
+    ];
+
+    /* property */
+    public function property()
+    {
+        return $this->belongsTo(Property::class, 'property_id', 'property_id');
+    }
+
+    /* unit */
+    public function unit()
+    {
+        return $this->belongsTo(Unit::class, 'unit_id', 'unit_id');
+    }
 }
--- database/migrations/2023_04_13_063723_create_tenants_table.php
+++ database/migrations/2023_04_13_063723_create_tenants_table.php
@@ -14,7 +14,37 @@ class CreateTenantsTable extends Migration
     public function up()
     {
         Schema::create('tenants', function (Blueprint $table) {
-            $table->id();
+            $table->id('tenant_id');
+            $table->string('first_name')->required();
+            $table->string('last_name')->required();
+            $table->integer('contact_number')->required();
+            $table->string('job')->nullable();
+            $table->integer('age');
+            $table->integer('familly_member')->required();
+            $table->string('email')->required();
+            $table->string('password')->required();
+            $table->string('p_address')->nullable();
+            $table->string('p_country')->nullable();
+            $table->string('p_state')->nullable();
+            $table->string('p_city')->nullable();
+            $table->string('p_zip_code')->nullable();
+
+            $table->string('present_address')->nullable();
+            $table->string('present_country')->nullable();
+            $table->string('present_state')->nullable();
+            $table->string('present_city')->nullable();
+            $table->string('present_zip_code')->nullable();
+
+            $table->integer('property_id')->required();
+            $table->integer('unit_id')->required();
+            $table->string('lease_start_date')->required();
+            $table->string('lease_end_date')->required();
+            $table->integer('general_rent')->required();
+            $table->integer('security_deposit')->nullable();
+            $table->integer('late_fee')->nullable();
+            $table->integer('incident_recipt')->nullable();
+            $table->string('payment_due_on_date')->nullable();
+            $table->string('document')->required();
             $table->timestamps();
         });
     }
