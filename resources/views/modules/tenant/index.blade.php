dfd28253003d2c3a77b18ce1d42d2c0eddfad877
Bokhtiar <bokhtiartoshar1@gmail.com>
2023-04-16 14:40:03 +0600

    tenant list updated

--- resources/views/modules/tenant/index.blade.php
+++ resources/views/modules/tenant/index.blade.php
@@ -26,56 +26,59 @@
             @foreach ($tenants as $item)
                 <div class="col-12 col-sm-3 col-lg-3 col-md-3 ">
                     <div class="card px-3 py-3" style="width: 22rem;">
-                        {{-- name information --}}
-                        <div class="flex items-center justify-between">
-                            <div class="flex gap-2">
-                                <img src="/images/{{$item->image}}" height="30px" width="70px" class="  rounded-full" alt="">
-                                <p>
-                                    <span>asdfsda</span><br>
-                                    <span>019912121212</span>
-                                </p>
-                            </div>
-                            <a href="">edit</a>
+                        {{-- name image email --}}
+
+                        <img src="/images/{{ $item->image }}" height="70px" width="80px" class="mx-auto  rounded-full"
+                            alt="">
+                        <div class="mx-auto">
+                            <span>{{ $item->first_name . ' ' . $item->last_name }}</span><br>
+                            <span>{{ $item->contact_number }}</span>
+                            <p class="mx-auto">
+                                <a href="" class="btn btn-sm btn-success"><i
+                                        class="ri-delete-bin-6-fill"></i></a>
+                                <a href="" class="btn btn-sm btn-danger"><i class="ri-edit-box-fill"></i></a>
+                            </p>
                         </div>
+
+
 
                         {{-- soft information --}}
                         <div class=" my-3">
 
-                       
-                        <p class="flex items-center gap-3">
-                            <span>Email: </span>
-                            <span class="text-gray-500">{{$item->email}}</span>
-                        </p> 
 
-                        <p class="flex items-center gap-3">
-                            <span>Property: </span>
-                            <span class="text-gray-500">{{$item->Property ? $item->property->name : ""}}</span>
-                        </p> 
+                            <p class="flex justify-between items-center gap-3 bg-gray-100 py-2 px-2 rounded">
+                                <span class="text-gray-500">Email: </span>
+                                <span class="text-gray-500">{{ $item->email }}</span>
+                            </p>
 
-                        <p class="flex items-center gap-3">
-                            <span>Unit: </span>
-                            <span class="text-gray-500">{{$item->unit ? $item->unit->unit_name : ""}}</span>
-                        </p> 
+                            <p class="flex justify-between items-center gap-3 bg-gray-100 py-2 px-2 rounded">
+                                <span class="text-gray-500">Property: </span>
+                                <span class="text-gray-500">{{ $item->Property ? $item->property->name : '' }}</span>
+                            </p>
 
-                        <p class="flex items-center gap-3">
-                            <span class="">Last rent paid: </span>
-                            <span class="text-gray-500">N/A</span>
-                        </p> 
+                            <p class="flex justify-between items-center gap-3 bg-gray-100 py-2 px-2 rounded">
+                                <span class="text-gray-500">Unit: </span>
+                                <span class="text-gray-500">{{ $item->unit ? $item->unit->unit_name : '' }}</span>
+                            </p>
 
-                        <p class="flex items-center gap-3">
-                            <span>Current rent: </span>
-                            <span class="text-gray-500">3000Tk</span>
-                        </p> 
+                            <p class="flex justify-between items-center gap-3 bg-gray-100 py-2 px-2 rounded">
+                                <span class="text-gray-500">Last rent paid: </span>
+                                <span class="text-gray-500">N/A</span>
+                            </p>
 
-                        <p class="flex items-center gap-3">
-                            <span>Previous Due: </span>
-                            <span class="text-gray-500">3000Tk</span>
-                        </p> 
-                    </div>
-                        <a href="@route('property.show', $item->property_id)" class=" btn btn-primary w-full">View Details</a>        
+                            <p class="flex justify-between items-center gap-3 bg-gray-100 py-2 px-2 rounded">
+                                <span class="text-gray-500">Current rent: </span>
+                                <span class="text-gray-500">3000Tk</span>
+                            </p>
+
+                            <p class="flex items-center justify-between gap-3 bg-gray-100 py-2 px-2 rounded">
+                                <span class="text-gray-500">Previous Due: </span>
+                                <span class="text-gray-500">3000Tk</span>
+                            </p>
+                        </div>
+                        <a href="@route('property.show', $item->property_id)" class=" btn btn-primary w-full">View Details</a>
                     </div>
                 </div>
-                
             @endforeach
 
         </div>
