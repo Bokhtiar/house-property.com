3e7c2b7f52e6127f93bfb0bf4330654393ae476c
Bokhtiar <bokhtiartoshar1@gmail.com>
2023-05-10 14:45:46 +0600

    bill model and form validation created

---
+++ app/Models/Bill.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bill extends Model
+{
+    use HasFactory;
+    protected $table = 'bills';
+    protected $primaryKey = 'bill_id';
+
+    protected $fillable = [
+        'property_id',
+        'unit_id',
+        'bill_pay_date',
+        'created_at',
+        'notes',
+    ];
+
+    /* property releation */
+    public function property()
+    {
+        return $this->belongsTo(Property::class, 'property_id', 'property_id');
+    }
+
+    /* unit releation */
+    public function unit()
+    {
+        return $this->belongsTo(Unit::class, 'unit_id', 'unit_id');
+    }
+}
---
+++ database/migrations/2023_05_10_083448_create_bills_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id('bill_id');
+            $table->integer('property_id')->required();
+            $table->integer('unit_id')->required();
+            $table->date('bill_pay_date')->required();
+            $table->integer('create_at')->required();
+            $table->longText('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bills');
+    }
+}
